2387e44ed467f2b216126b9485c8b793e87fe215
منيره حسن العقيلي <malaqili2004@gmail.com>
2022-08-14 21:31:20 +0300

    is done

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ alert(`WELLCOME TO MY SITE`)
 
 let  grade =prompt(`اكتب درجتك بالارقام`)
 
-console.log(`grade`)
 
- console.log ( "%cgrade", "color: orange");
+console.log ( "%cgrade", "color: orange");
 
+console.log(grade);
 
 if( grade>=90 && grade<=100  ){
-console.log(`لقد حصلت على امتياز `)
+console.log(`%cلقد حصلت على امتياز ` , `color:orange`)
 }else if(grade >=80 && grade <=89){
-    console.log(`لقد حصلت على جيد جدا` )
+    console.log(`%cلقد حصلت على جيد جدا` , `color:green` )
 }else if(grade >=70 && grade<=79 ){
 
-    console.log(`لقد حصلت على جيد `)
+    console.log(`%cلقد حصلت على جيد ` , `color:pink`)
 
 }else if(grade >=60 && grade <=69){
 
-    console.log(`لقد حصلت على مقبول`)
+    console.log(`%cلقد حصلت على مقبول` , `color:blue`)
 }else if(grade>=50 && grade<=59){
 
-    console.log(`لقد حصلت على ضعيف `)
+    console.log(`%cلقد حصلت على ضعيف ` , `color:red`)
 }else{
     console.log(`راسب`)
 
